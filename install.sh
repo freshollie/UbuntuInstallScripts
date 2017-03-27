7e7636ea79abddab0e5673abdfc6d7a01d850a00
Oliver Bell <freshollie@gmail.com>
2017-03-28 00:51:11 +0100

    Added check for system type

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
-# Script to install some applications on trustylxde 
+# Script to install some applications on ubuntu 
 
+DESKTOP=false
+read -n1 -p "Desktop?(y, n): " doit 
+echo""
+case $doit in  
+  y|Y) DESKTOP=true;; 
+  *);; 
+esac
 
 sudo apt-get -y install software-properties-common python-software-properties
-
 
 # Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
@@ -14,8 +20,10 @@ sudo add-apt-repository -y ppa:mystic-mirage/pycharm
 # Notepad QQ
 sudo add-apt-repository -y ppa:notepadqq-team/notepadqq
 
-# Touchpad indicator
-sudo add-apt-repository -y ppa:atareao/atareao 
+if [ $DESKTOP == false ]; then
+	# Touchpad indicator
+	sudo add-apt-repository -y ppa:atareao/atareao 
+fi
 
 # F.lux
 sudo add-apt-repository -y ppa:nathan-renniewaldock/flux
@@ -45,7 +53,10 @@ sudo apt-get -y install pycharm
 
 sudo apt-get -y install notepadqq
 
-sudo apt-get -y install touchpad-indicator
+# Laptop needs touchpad
+if [DESKTOP == false]; then
+	sudo apt-get -y install touchpad-indicator
+fi
 
 sudo apt-get -y install fluxgui
 
@@ -62,7 +73,14 @@ sudo apt-get -y install g++
 sudo apt-get -y install android-tools-adb android-tools-fastboot
 
 sudo apt-get -y install oracle-java8-installer
-sudo apt-get -y install lib32stdc++6
+
+# Desktop needs these set for AVD
+if [ $DESKTOP == true ]; then
+	sudo apt-get -y install lib32stdc++6 lib32z1
+	sudo apt-get -y install libgl1-mesa-dev
+	sudo echo "export ANDROID_EMULATOR_USE_SYSTEM_LIBS=1">>$HOME/.profile
+fi 
+
 sudo apt-get -y install ubuntu-make
 sudo umake android android-studio
 
